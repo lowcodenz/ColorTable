1ae13c9216d3ee254d68049a304a0a160d62a507
jasonteunissen <jasonteunissen@gmail.com>
2023-06-08 12:23:32 +1200

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,25 @@
+// all the variables here
+
+//an array here of all the values
+const shadevalue = [900, 700, 500, '', 300, 100, 90, 70, 50, 30, 10];
+
+// another array of all the semantic elements
+const color = ['primary', 'secondary','accent','success','warning','danger','clickable','active','info','inverse','default','text', 'header'];
+
+
+
+// what the html should look like
+
+<table>
+  <tr>
+    <th>color</th>                  //for each shadevalue make on of these
+    <th></th>                       //for each shadevalue make on of these
+  </tr>
+  <tr>                              //for each color make on of these
+    <td>color name</td>                       //for each shadevalue make on of these
+    <td></td>                       //for each shadevalue make on of these
+  </tr>
+
+</table>
+
+
